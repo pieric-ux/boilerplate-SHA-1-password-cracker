83f3406fe7e83ef698f125adee9fe42463cb62f4
pieric-ux <27101601-pieric-ux@users.noreply.replit.com>
2023-12-16 15:05:20 +0000

    Initial commit

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,47 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+
+def read_passwords(filename):
+  passwords = []
+  with open(filename, 'r') as file:
+    for password in file:
+      passwords.append(password.strip())
+  return passwords
+
+
+def read_salts(filename):
+  salts = []
+  with open(filename, 'r') as file:
+    for salt in file:
+      salts.append(salt.strip())
+  return salts
+
+
+def crack_sha1_hash(hash, use_salts=False):
+  file_passwords = 'top-10000-passwords.txt'
+  file_salts = 'known-salts.txt'
+
+  passwords = read_passwords(file_passwords)
+
+  if use_salts:
+    salts = read_salts(file_salts)
+
+    for salt in salts:
+      for password in passwords:
+        salted_password_prepend = password + salt
+        hashed_password_prepend = hashlib.sha1(salted_password_prepend.encode()).hexdigest()
+        
+        salted_password_append = salt + password
+        hashed_password_append = hashlib.sha1(salted_password_append.encode()).hexdigest()
+
+        if hashed_password_prepend == hash or hashed_password_append == hash:
+          return password
+
+  else:
+    for password in passwords:
+      hashed_password = hashlib.sha1(password.encode()).hexdigest()
+
+      if hashed_password == hash:
+        return password
+
+  return 'PASSWORD NOT IN DATABASE'
